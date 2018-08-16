0dc1d93763caf0c60e24865c2be5c6c28ef32c75
Psychopath <sn05coast@gmail.com>
2018-08-16 11:30:28 +0300

    без звёздочки

---
+++ src/towns.js
@@ -0,0 +1,123 @@
+/*
+ Страница должна предварительно загрузить список городов из
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ и отсортировать в алфавитном порядке.
+
+ При вводе в текстовое поле, под ним должен появляться список тех городов,
+ в названии которых, хотя бы частично, есть введенное значение.
+ Регистр символов учитываться не должен, то есть "Moscow" и "moscow" - одинаковые названия.
+
+ Во время загрузки городов, на странице должна быть надпись "Загрузка..."
+ После окончания загрузки городов, надпись исчезает и появляется текстовое поле.
+
+ Разметку смотрите в файле towns-content.hbs
+
+ Запрещено использовать сторонние библиотеки. Разрешено пользоваться только тем, что встроено в браузер
+
+ *** Часть со звездочкой ***
+ Если загрузка городов не удалась (например, отключился интернет или сервер вернул ошибку),
+ то необходимо показать надпись "Не удалось загрузить города" и кнопку "Повторить".
+ При клике на кнопку, процесс загруки повторяется заново
+ */
+
+/*
+ homeworkContainer - это контейнер для всех ваших домашних заданий
+ Если вы создаете новые html-элементы и добавляете их на страницу, то дабавляйте их только в этот контейнер
+
+ Пример:
+   const newDiv = document.createElement('div');
+   homeworkContainer.appendChild(newDiv);
+ */
+const homeworkContainer = document.querySelector('#homework-container');
+/*
+ Функция должна вернуть Promise, который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+ */
+
+function loadTowns() {
+    return new Promise(function (resolve, reject) {
+        var towns = [];
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json');
+        xhr.send();
+        xhr.addEventListener('load', () => {
+            if (xhr.status >= 400) {
+                reject();
+            } else {
+                var townsObj = JSON.parse(xhr.responseText)
+
+                for (var key in townsObj) {
+                    towns.push(townsObj[key])
+                }
+                towns.sort(function (obj1, obj2) {
+                    if (obj1.name < obj2.name) { 
+                        return -1;
+                    }
+                    if (obj1.name > obj2.name) { 
+                        return 1;
+                    }
+                    
+                    return 0;
+                });
+                
+                return resolve(towns)
+            }
+        })
+    })
+}
+/*
+Функция должна проверять встречается ли подстрока chunk в строке full
+Проверка должна происходить без учета регистра символов
+
+Пример:
+isMatching('Moscow', 'moscow') // true
+isMatching('Moscow', 'mosc') // true
+isMatching('Moscow', 'cow') // true
+isMatching('Moscow', 'SCO') // true
+isMatching('Moscow', 'Moscov') // false
+*/
+function isMatching(full, chunk) {
+    return full.toLowerCase().includes(chunk.toLowerCase())
+}
+
+/* Блок с надписью "Загрузка" */
+const loadingBlock = homeworkContainer.querySelector('#loading-block');
+/* Блок с текстовым полем и результатом поиска */
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+/* Текстовое поле для поиска по городам */
+const filterInput = homeworkContainer.querySelector('#filter-input');
+/* Блок с результатами поиска */
+const filterResult = homeworkContainer.querySelector('#filter-result');
+
+loadTowns()
+    .then(loadingBlock.style.display = 'none')
+    .then(filterBlock.style.display = 'block')
+    .then(function(value) {
+        filterInput.addEventListener('keyup', function() {
+            var result = [];
+
+            filterResult.innerHTML = '';
+
+            for (var key in value) {
+                result.push(value[key].name);
+            }
+            result.forEach(function(item) {
+                if (isMatching(item, filterInput.value)) {
+                    var div = filterResult.appendChild(document.createElement('div'))
+
+                    div.innerHTML = item;
+                } 
+                if (filterInput.value == 0) {
+                    filterResult.innerHTML = '';
+                }
+            })
+        });
+    })
+
+export {
+    loadTowns,
+    isMatching
+};
